88c38a0b00b391c810512894b85e6cd64842083c
kyudong.kim@oracle.com <xers989@gmail.com>
2021-06-23 14:33:24 +0900

    amend tf

--- ocivcn/main.tf
+++ ocivcn/main.tf
@@ -19,3 +19,11 @@ resource "oci_core_vcn" "internal" {
   display_name   = "My internal VCN"
 }
 
+resource "oci_core_subnet" "dev" {
+  vcn_id                      = oci_core_vcn.internal.id
+  cidr_block                  = "172.16.0.0/24"
+  compartment_id              = "ocid1.compartment.oc1..aaaaaaaaibxp2h4wo5hioiexhgfxnnbmmupnqs56retlbjux7qwchziyntcq"
+  display_name                = "Dev subnet 1"
+  prohibit_public_ip_on_vnic  = true
+  dns_label                   = "dev"
+}
